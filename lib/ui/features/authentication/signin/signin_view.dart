6d998d31e4d8355234e3950db52b684ec54ef90b
ahsuunn <themalique1910@gmail.com>
2025-03-29 23:01:09 +0700

    modify : sign in styling

--- lib/ui/features/authentication/signin/signin_view.dart
+++ lib/ui/features/authentication/signin/signin_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:founded_ninu/data/services/auth_provider.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SigninPage extends ConsumerWidget {
   SigninPage({super.key});
@@ -14,18 +15,57 @@ class SigninPage extends ConsumerWidget {
 
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            bool success = await authService.signIn(
-              "user@example.com",
-              "password123",
-            );
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.asset("assets/logo.png"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          "NINU",
+                          style: TextStyle(
+                            fontFamily: 'DinNextW1G',
+                            fontSize: 82,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Login to your account",
+                    style: GoogleFonts.lato(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      bool success = await authService.signIn(
+                        "user@example.com",
+                        "password123",
+                      );
 
-            if (success && context.mounted) {
-              context.go('/home/Guest');
-            }
-          },
-          child: Text("Sign In"),
+                      if (success && context.mounted) {
+                        context.go('/home/Guest');
+                      }
+                    },
+                    child: Text("Sign In"),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
